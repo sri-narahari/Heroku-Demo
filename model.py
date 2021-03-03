99784db26aaa5a3e560df35897cc91086679d8a1
sri-narahari <75279155+sri-narahari@users.noreply.github.com>
2021-03-03 15:37:00 -0500

    Add files via upload

---
+++ model.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 16:14:53 2021
+
+@author: SNARAHA1
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+dataset=pd.read_csv('hiring.csv')
+dataset['experience'].fillna(0,inplace=True)
+dataset['test_score'].fillna(dataset['test_score'].mean(),inplace=True)
+
+X=dataset.iloc[:,:-1]
+X
+
+def convert_to_int(word):
+    word_dict={'one':1, 'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,
+               'ten':10,'eleven':11,'tweleve':12,'zero':0,0:0}
+    return word_dict[word]
+
+X['experience']=X['experience'].apply(lambda x : convert_to_int(x))
+X
+
+y=dataset.iloc[:,-1]
+
+from sklearn.linear_model import LinearRegression
+regressor= LinearRegression()
+
+regressor.fit(X,y)
+
+#saving model to disk
+pickle.dump(regressor,open('model.pkl','wb'))
+
+#loading model to compare results
+
+model=pickle.load(open('model.pkl','rb'))
+print(model.predict([[2,9,6]]))
